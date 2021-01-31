ff3e9ee720d5cacd217fb0b5ddcba416f1cc3b17
ChenCyl <programming2@163.com>
2021-02-01 00:46:16 +0800

    take the leap

---
+++ sidebars.js
@@ -0,0 +1,13 @@
+module.exports = {
+  someSidebar: {
+    // Docusaurus: ['doc1', 'doc2', 'doc3'],
+    // Features: ['mdx'],
+    Introduction: [
+      'Introduction/doc1'
+    ],
+    'English Learning': [
+      'English-Learning/note2',
+      'English-Learning/note1'
+    ]
+  },
+};
